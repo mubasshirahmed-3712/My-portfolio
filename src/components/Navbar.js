453eee880f1a3666870847641a2af679fd3ca6ea
Mubasshir Ahmed <mubasshir3712@gmail.com>
2025-04-13 20:30:33 +0530

    changes in new homepage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,6 @@ import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
-import { ImBlog } from "react-icons/im";
 import {
   AiFillStar,
   AiOutlineHome,
@@ -95,15 +94,7 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item>
-              <Nav.Link
-                href="https://soumyajitblogs.vercel.app/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <ImBlog style={{ marginBottom: "2px" }} /> Blogs
-              </Nav.Link>
-            </Nav.Item>
+            
 
             <Nav.Item className="fork-btn">
               <Button
